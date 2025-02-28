ef7bab0a9a1abd65fe93456dbc9efc0ede7b2613
VikaMokhovikova <vika.mokhovikova@gmail.com>
2025-02-28 23:34:54 +0000

    Function for data preprocessing

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,1 +1,17 @@
-def clean_none
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from plotly.subplots import make_subplots
+
+def data_preprocessing(data: pd.DataFrame) -> tuple:
+    """
+    :param data: dataframe with data
+    :return: 4 dataframes X_train, X_test, y_train, y_test
+    """
+    data['GENDER'] = data['GENDER'].replace({'M': 0, 'F': 1})
+    data['LUNG_CANCER'] = data['LUNG_CANCER'].replace({'NO': 0, 'YES': 1})
+    X = data.drop(columns=['LUNG_CANCER'])
+    y = data['LUNG_CANCER']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test
